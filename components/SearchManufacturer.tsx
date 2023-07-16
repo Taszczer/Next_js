ff6044d9c94a353c30343bfb706e5f51c9ea14e2
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-16 18:19:40 +0200

    SearchBar

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { useState, Fragment } from 'react'
 import { SearchManufacturerProps } from '@/types'
 import { Combobox, Transition} from '@headlessui/react'
 import Image from 'next/image'
 
 const SearchManufacturer = ({ manufacturer, setManufacturer}: SearchManufacturerProps ) => {
+  const [query, setQuery] = useState('')
+  
   return (
     <div className='search-manufacturer'>
       <Combobox> 
@@ -18,7 +21,22 @@ const SearchManufacturer = ({ manufacturer, setManufacturer}: SearchManufacturer
               className='ml-4'
             />
           </Combobox.Button>
-          
+
+          <Combobox.Input
+            className='search-manufacturer__input'
+            placeholder='Volkswagen'
+            displayValue={(manufacturer:string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition
+            as={Fragment}
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+
+          </Transition>
         </div>  
       </Combobox>
     </div>
