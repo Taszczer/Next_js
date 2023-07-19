5985335631d3ce9b469955e433f6856a7561e511
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-19 20:13:41 +0200

    demo cars

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -40,7 +40,36 @@ const CarCard = ({ car }: CarCardProps) => {
                   className="object-contain z-0"
               />
           </div>
-      </div>
+          <div className="relative flex w-full mt-2">
+              <div className="flex group-hover:invisible w-full justify-between text-gray">
+                  <div className="flex flex-col justify-center items-center gap-2">
+                      <Image src='/steering-wheel.svg' width={20} height={20} alt="steering wheel" />
+                      <p className="text-[14px]">
+                          {transmission === 'a' ? 'Automatic': 'Manual'}
+                      </p>
+                  </div>
+                  <div className="flex flex-col justify-center items-center gap-2">
+                      <Image src='/tire.svg' width={20} height={20} alt="tire" />
+                      <p className="text-[14px]">
+                          {drive.toUpperCase()}
+                      </p>
+                  </div>
+                  <div className="flex flex-col justify-center items-center gap-2">
+                      <Image src='/gas.svg' width={20} height={20} alt="gas" />
+                      <p className="text-[14px]">
+                          {city_mpg} MPG
+                      </p>
+                  </div>
+              </div>
+
+              <button className="car-card__btn-container">
+                  <CustomButton
+                      title='View more'
+                      containerStyles='text-white w-full py-[16px] rounded-full bg-primary-blue'
+                  />
+              </button>
+           </div>
+        </div>
   )
 }
 
