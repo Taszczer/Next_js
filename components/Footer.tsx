f6615592e56a424dc719f4d290ac70b45ed21e64
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-19 11:49:13 +0200

    SearchBar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { footerLinks } from '@/constants'
 
 const Footer = () => {
   return (
-    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+    <footer className='flex flex-col text-black-100  border-t border-gray-100 mt-5'>
           <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
               <div className='flex flex-col justify-start items-start gap-6'>
                   <Image
@@ -14,7 +14,7 @@ const Footer = () => {
                       height={18}
                       className='object-contain'
                   />
-                  <p className='text-base text-gray-700'> Carnhub 2023 <br /> All rights reserved &copy;</p>
+                  <p className='text-base text-gray-700'> Carhub 2023 <br /> All rights reserved &copy;</p>
               </div>
 
               <div className='footer__links '>
