7f1d6ee7863cb713e7571eceda21131e1fa4af42
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-20 19:45:16 +0200

    new dialog for cars

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -32,7 +32,7 @@ const CarCard = ({ car }: CarCardProps) => {
                   /day
               </span>
           </p>
-
+{/* xD */}
           <div className="relative w-full h-40 my-3 object-contain">
               <Image
                   src='/hero.png'
--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { CarProps } from "@/types"
 import Image from "next/image"
 import { Fragment } from "react"
@@ -14,7 +16,19 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
       <>
         <Transition appear show={isOpen} as={Fragment}>
-            <Dialog as="div" className='relative z-10' onClose={closeModal}></Dialog>
+              <Dialog as="div" className='relative z-50' onClose={closeModal}>
+                  <Transition.Child
+                      as={Fragment}
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      enter="ease-out duration-300"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                  >
+                    <div className="fixed inset-0 bg-black bg-opacity-25"/>
+                  </Transition.Child>  
+              </Dialog>
         </Transition>
       </>
   )
