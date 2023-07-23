491c88abda16443f8486603c897312d6ea6d2820
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-23 15:08:13 +0200

    search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,12 +3,42 @@
 import { useState } from "react"
 import { SearchManufacturer } from "./"
 import Image from 'next/image'
+import {  useRouter } from "next/navigation"
 
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('')
-  const [model, setModel] =useState('')
-    const handleSearch = () => {}
+  const [model, setModel] = useState('')
+  const router = useRouter()
+    
+  const handleSearch = (e: React.FormEvent<HTMLElement>) => { 
+    e.preventDefault()
+
+    if (manufacturer === '' && model === '') {
+      return alert('Please fill in the search bar')
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+  }
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search)
+
+    if (model) {
+      searchParams.set('model', model)
+    } else {
+      searchParams.delete('model')
+    }
+
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer)
+    } else {
+      searchParams.delete('manufacturer')
+    }
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+    router.push(newPathname)
+  }
 
   const SearchButton = ({ otherClasses } : { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -23,16 +53,17 @@ const SearchBar = () => {
   )
   
   return (
-      <form
+      <form 
           className='searchbar'
-          onSubmit={handleSearch}>
+          onSubmit={handleSearch}
+      >
             <div className="searchbar__item">
               <SearchManufacturer
                 manufacturer={manufacturer}
                 setManufacturer={setManufacturer}
-            />  
-            <SearchButton otherClasses="sm:hidden"/>
-      </div>
+              />  
+              <SearchButton otherClasses="sm:hidden"/>
+            </div>
       <div className="searchbar__item">
         <Image
           src='/model-icon.png'
