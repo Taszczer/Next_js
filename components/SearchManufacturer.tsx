5614e2357c3a47108f5443cee4f93093fac7b888
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-23 15:24:08 +0200

    search bar

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -23,7 +23,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
     <div className='search-manufacturer '>
       <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
-          {/* Button for the combobox. Click on the icon to see the complete dropdown */}
+          
           <Combobox.Button className='absolute top-[14px]'>
             <Image
               src='/car-logo.svg'
@@ -34,7 +34,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
             />
           </Combobox.Button>
 
-          {/* Input field for searching */}
+         
           <Combobox.Input
             className='search-manufacturer__input'
             displayValue={(item: string) => item}
@@ -42,13 +42,13 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
             placeholder='Volkswagen...'
           />
 
-          {/* Transition for displaying the options */}
+          
           <Transition
-            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            as={Fragment} 
             leave='transition ease-in duration-100'
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
-            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
+            afterLeave={() => setQuery("")} 
           >
             <Combobox.Options
               className='z-50 overflow-auto absolute mt-1 max-h-60 w-full rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
@@ -78,7 +78,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                           {item}
                         </span>
 
-                        {/* Show an active blue background color if the option is selected */}
+                        
                         {selected ? (
                           <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
                           ></span>
