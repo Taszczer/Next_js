289f26bcb6cb34ed90d69c3ce96ca1620b4613a7
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-23 13:12:38 +0200

    seaechbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,17 +2,26 @@
 
 import { useState } from "react"
 import { SearchManufacturer } from "./"
+import Image from 'next/image'
 
-const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
-  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
-    
-  </button>
-)
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] =useState('')
+  const [manufacturer, setManufacturer] = useState('')
+  const [model, setModel] =useState('')
     const handleSearch = () => {}
 
+  const SearchButton = ({ otherClasses } : { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+      <Image
+        src='/magnifying-glass.svg'
+        alt='maginifying glass'
+        width={40}
+        height={40}
+        className='object-contain'
+      />
+  </button>
+  )
+  
   return (
       <form
           className='searchbar'
@@ -23,8 +32,28 @@ const SearchBar = () => {
                 setManufacturer={setManufacturer}
             />  
             <SearchButton otherClasses="sm:hidden"/>
-            </div>
-      </form>
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src='/model-icon.png'
+          alt='car model'
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan..."
+          className="searchbar__input"
+        />
+
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
+    </form>
   )
 }
 
