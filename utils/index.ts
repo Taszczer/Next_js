976f047aa9b13b0eca6d24338126d8d9956138dc
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-24 20:41:57 +0200

    search bar

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 import { CarProps } from "@/types";
 
-export async function  fetchCars() {
+export async function fetchCars() {
     const headers = {
 		'X-RapidAPI-Key': '650fb79ca9mshba0792dddafda81p1f4bd4jsn43bcbc367a30',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
