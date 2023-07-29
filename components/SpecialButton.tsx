7a95b3129b3cd188e527f1efb76afa02abcf9a8e
lolkek1234567890 <dima2019mega@gmail.com>
2023-07-29 15:34:48 +0200

    close button

--- components/SpecialButton.tsx
+++ components/SpecialButton.tsx
@@ -2,6 +2,7 @@
 
 import { forSpecialButton } from '@/types'
 import { Dialog, Transition } from '@headlessui/react'
+import Image from 'next/image';
 import { Fragment, useState } from 'react'
 
 
@@ -53,6 +54,34 @@ const SpecialButton = ({title, containerStyles, btnType, isDisabled, textStyles,
           >
             <div className='fixed bg-black bg-opacity-25 inset-0' />
           </Transition.Child>
+          <div className='fixed inset-0 overflow-y-auto'>
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter=' ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo=' opacity-100 scale-100'
+                leave=' ease-in duration-200'
+                leaveFrom=' opacity-100 scale-100'
+                leaveTo=' opacity-0 scale-0'
+              >
+                <Dialog.Panel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+                  <button
+                    type='button'
+                    onClick={closeM}
+                  >
+                    <Image
+                      src="/close.svg"
+                      alt="close"
+                      className='object-contain'
+                      width={50}
+                      height={50}
+                    />
+                  </button>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
         </Dialog>
       </Transition>
     </>
